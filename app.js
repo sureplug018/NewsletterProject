87fb9b14444f0a4db39de56d172dfdffe79557c5
Raphael Eze <ezea5149@gmail.com>
2023-03-20 05:38:07 +0100

    add app.js and Procfile

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("app is running on port 3000....");
 });
 
